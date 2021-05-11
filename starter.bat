8bba3ed359e3dc91a7ea0cbcc77f92dfaccbaafe
Ruijie Geng <ruijiegeng@gmail.com>
2021-05-11 15:59:11 -0400

    Update starter.bat

--- starter.bat
+++ starter.bat
@@ -2,8 +2,10 @@
 set appPath=%1
 set queue=%2
 set dst=%3
+set thread=%4
 
 Title %queue%
 :while1
-    C:\Users\ruijie\AppData\Local\chia-blockchain\%appPath%\resources\app.asar.unpacked\daemon\chia.exe plots create -k 32 -b 4750 -r 4 -u 128 -t D:\%queue% -d %dst% >> log\%queue%.log
+    %LOCALAPPDATA%\chia-blockchain\%appPath%\resources\app.asar.unpacked\daemon\chia.exe plots create -k 32 -b 4750 -r %thread% -u 128 -t D:\%queue% -d %dst% >> log\%queue%.log
+    ping 127.0.0.1 -n 10 >nul
     goto :while1
