05b94e689009e3f226c7c4188286d9881a116988
Ruijie Geng <ruijiegeng@gmail.com>
2021-05-11 23:11:36 -0400

    Update plot.bat

--- plot.bat
+++ plot.bat
@@ -1,25 +1,12 @@
+set appPath=%1
+set queue=%2
+set temp=%3
+set dst=%4
+set thread=%5
 
-set appPath=app-1.1.5
+Title %queue%
+:while1
+    %LOCALAPPDATA%\chia-blockchain\%appPath%\resources\app.asar.unpacked\daemon\chia.exe plots create -k 32 -b 4750 -r %thread% -u 128 -t D:\%queue% -d %dst%  >> log\%queue%.log
+    ping 127.0.0.1 -n 10 >nul
+    goto :while1
 
-rem 4 arguments: 
-rem  - appPath 
-rem  - queue 
-rem  - dst 
-rem  - thread
-
-start  1-1.bat %appPath% 1-1 
-ping 127.0.0.1 -n 800 >nul
-
-start  1-2.bat %appPath% 1-2
-ping 127.0.0.1 -n 10000 >nul
-
-start  1-3.bat %appPath% 1-3
-ping 127.0.0.1 -n 800 >nul
-
-start  1-4.bat  %appPath% 1-4
-ping 127.0.0.1 -n 10000 >nul
-
-start  1-5.bat %appPath% 1-5
-ping 127.0.0.1 -n 800 >nul
-
-start  1-6.bat  %appPath% 1-6
