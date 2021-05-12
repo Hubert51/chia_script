32ae3fe13be12c0dba2138d31e62b91cbfc38cc2
Ruijie Geng <ruijiegeng@gmail.com>
2021-05-11 23:11:54 -0400

    Update starter.bat

--- starter.bat
+++ starter.bat
@@ -1,11 +1,43 @@
-@echo off
-set appPath=%1
-set queue=%2
-set dst=%3
-set thread=%4
 
-Title %queue%
-:while1
-    %LOCALAPPDATA%\chia-blockchain\%appPath%\resources\app.asar.unpacked\daemon\chia.exe plots create -k 32 -b 4750 -r %thread% -u 128 -t D:\%queue% -d %dst% >> log\%queue%.log
-    ping 127.0.0.1 -n 10 >nul
-    goto :while1
+set appPath=app-1.1.5
+
+rem 4 arguments: 
+rem  - appPath 
+rem  - queue 
+rem  - temp folder
+rem  - dst 
+rem  - thread
+
+set thread=4
+
+
+start  plot.bat %appPath% 1-1 D: F: %thread%
+ping 127.0.0.1 -n 800 >nul
+
+start  plot.bat %appPath% 1-2 E: F: %thread%
+ping 127.0.0.1 -n 800 >nul
+
+start  plot.bat %appPath% 1-3 D: F: %thread%
+ping 127.0.0.1 -n 800 >nul
+
+start  plot.bat %appPath% 1-4 D: F: %thread%
+ping 127.0.0.1 -n 10000 >nul
+
+
+start  plot.bat %appPath% 1-5 D: F: %thread%
+ping 127.0.0.1 -n 800 >nul
+
+start  plot.bat %appPath% 1-6 D: F: %thread%
+ping 127.0.0.1 -n 800 >nul
+
+start  plot.bat %appPath% 1-7 E: F: %thread%
+ping 127.0.0.1 -n 800 >nul
+
+start  plot.bat %appPath% 1-8 D: F: %thread%
+ping 127.0.0.1 -n 10000 >nul
+
+
+start  plot.bat %appPath% 1-9 D: F: %thread%
+ping 127.0.0.1 -n 800 >nul
+
+start  plot.bat %appPath% 1-10 E: F: %thread%
